b430e1b6ef9837f206196d017dac87beb349d76a
m-swelam <mid2o0o11@gmail.com>
2021-10-30 13:12:11 +0200

    get data from strapi cms

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function Home() {
+import { url } from "../config/next.config";
+
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
       <h1>Welcome home!</h1>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>
+            <Link href={`/article/${item.id}`}>
+              <a>{item.Title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const data = await fetch(`${url}/articles`);
+  const list = await data.json();
+
+  return {
+    props: {
+      list,
+    },
+    revalidate: 1,
+  };
+};
